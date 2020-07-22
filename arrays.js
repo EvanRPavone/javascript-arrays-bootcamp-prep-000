33fba772c62b5b9aed69b788c104920fbf894f4b
Evan Pavone <evanrpavone@gmail.com>
2020-07-22 13:24:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,8 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
 function addElementToEndOfArray(chocolateBars, candyString) {
   return [...chocolateBars,candyString];
 }
+
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString) {
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
